1d6653f43502552824a1ca43a5e223944541ea23
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2022-08-07 20:24:03 +0200

    update js file

--- client/src/handlers/add-contact.js
+++ client/src/handlers/add-contact.js
@@ -6,23 +6,35 @@ export const addContact = (fullName) => {
 	if (fullName === '') alert('"Input is empty"');
 	if (!fullName || !fullName.trim()) return;
 
+	// created ul element
+
 	const contactUl = document.createElement('ul');
 	contactUl.setAttribute('class', 'contact-entry');
+
+	// created li for initial of name
 
 	const nameInitialLi = document.createElement('li');
 	nameInitialLi.setAttribute('class', 'name-initial');
 	nameInitialLi.textContent = fullName.charAt(0);
 	nameInitialLi.style.backgroundColor = randomColor();
 
+	// created li for fullname
+
 	const fullNameDiv = document.createElement('li');
 	fullNameDiv.setAttribute('class', 'full-name');
 	fullNameDiv.textContent = fullName;
 	fullNameDiv.style.backgroundColor = randomColor();
 
+	// append order
+
 	contactUl.appendChild(nameInitialLi);
 	contactUl.appendChild(fullNameDiv);
 
+	// assign id
+
 	document.getElementById(CONTACT_LIST).append(contactUl);
+
+	// store in inputs in the data file
 
 	state.addBook.push(contactUl);
 };
--- client/src/init.js
+++ client/src/init.js
@@ -1,4 +1,6 @@
 import { NAME_INPUT } from '../data/constants.js';
-
 import { listenersContact } from './listeners/add-contact.js';
+
+// add event listeners
+
 listenersContact(document.getElementById(NAME_INPUT));
